31054586feb4c4d0cf4a5707cb7cb0a209b3cb67
Arek Stopczynski <arekquest@gmail.com>
2013-05-29 15:15:06 +0000

    added cas fields to user model

--- sensible_data_platform/accounts/admin.py
+++ sensible_data_platform/accounts/admin.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from accounts.models import Student
+from accounts.models import Cas
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
-class StudentInline(admin.StackedInline):
-    model = Student
-    can_delete = False
-    verbose_name_plural = 'student'
+class CasInline(admin.StackedInline):
+    model = Cas
+    can_delete = True
+    verbose_name_plural = 'cas'
 
 # Define a new User admin
 class UserAdmin(UserAdmin):
-    inlines = (StudentInline, )
+    inlines = (CasInline, )
 
 # Re-register UserAdmin
 admin.site.unregister(User)
--- sensible_data_platform/accounts/models.py
+++ sensible_data_platform/accounts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Student(models.Model):
+class Cas(models.Model):
     user = models.OneToOneField(User)
-    student_id = models.CharField(max_length=100)
+    student_id = models.CharField(max_length=100, blank=True)
+    email = models.CharField(max_length=100, blank=True)
+    givenName = models.CharField(max_length=100, blank=True)
+    familyName = models.CharField(max_length=100, blank=True)
+    closed = models.CharField(max_length=100, blank=True)
