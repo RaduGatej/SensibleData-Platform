889b11c4cf4a4ce59d30e0ce7fc0965ac312d551
Arek Stopczynski <arekquest@gmail.com>
2013-06-03 17:57:09 +0000

    added identities to admin

--- sensible_data_platform/accounts/admin.py
+++ sensible_data_platform/accounts/admin.py
@@ -3,18 +3,25 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 from accounts.models import Cas
+from accounts.models import Participant
 
-# Define an inline admin descriptor for Employee model
-# which acts a bit like a singleton
 class CasInline(admin.StackedInline):
     model = Cas
     can_delete = True
     verbose_name_plural = 'cas'
 
-# Define a new User admin
+
 class UserAdmin(UserAdmin):
     inlines = (CasInline, )
 
-# Re-register UserAdmin
+
+class ParticipantInline(admin.StackedInline):
+    model = Participant
+    can_delete = True
+    verbose_name_plural = 'participant'
+
+class UserAdmin(UserAdmin):
+	inlines = (ParticipantInline, )
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
