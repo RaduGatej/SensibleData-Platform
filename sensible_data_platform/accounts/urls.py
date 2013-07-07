d04d28d26626c6931e0844c9b4379e7525fd9a8b
Arek Stopczynski <arekquest@gmail.com>
2013-07-07 09:04:51 +0000

    wrapper over login

--- sensible_data_platform/accounts/urls.py
+++ sensible_data_platform/accounts/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
-    (r'^login/$', 'django.contrib.auth.views.login'),
-    (r'^logout/$', 'accounts.session.logout'),
-    (r'^register/$', 'accounts.register.register'),
+   # url(r'^login/$', 'django.contrib.auth.views.login', name = 'login'),
+    url(r'^login/$', 'accounts.views.do_login', name = 'login'),
+    url(r'^logout/$', 'accounts.session.logout', name = 'logout'),
+    url(r'^register/$', 'accounts.register.register'),
 )
