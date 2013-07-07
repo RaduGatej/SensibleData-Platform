c6929f4922876eda78546fee72382ae9b571e115
Arek Stopczynski <arekquest@gmail.com>
2013-07-07 09:11:17 +0000

    new logout logic including next

--- sensible_data_platform/accounts/session.py
+++ sensible_data_platform/accounts/session.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
@@ -7,12 +7,12 @@ from django.contrib.auth.models import User
 
 from oauth2app.models import Client, AccessRange
 
-
-
-@login_required
 def logout(request):
-    auth.logout(request)
-    return render_to_response(
-        'logout.html',
-        {},
-        RequestContext(request))
+    	next = request.REQUEST.get('next', '')
+	
+	if request.user.is_authenticated:
+		auth.logout(request)
+    
+	if next == '':
+		return render_to_response('logout.html', {}, RequestContext(request))
+	return redirect(next)
