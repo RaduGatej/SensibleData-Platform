4c86197ae1a4660ed637dbce30794244f38e790b
Arek Stopczynski <arekquest@gmail.com>
2013-07-09 17:47:04 +0000

    named urls

--- sensible_data_platform/render/urls.py
+++ sensible_data_platform/render/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^profile/', 'render.views.profile'),
-    url(r'^dashboard/?$', 'render.dashboard.dashboard'),
-    url(r'^authorizations/?$', 'render.authorizations.authorizations'),
+    url(r'^profile/', 'render.profile.profile', name='profile'),
+    url(r'^dashboard/?$', 'render.dashboard.dashboard', name='dashboard'),
+    url(r'^authorizations/?$', 'render.authorizations.authorizations', name='authorizations'),
     url(r'^home/?$', 'render.views.home', name='home'),
     url(r'^', 'render.views.home'),
 )
