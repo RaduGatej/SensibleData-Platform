a980cc01689728b03deb93086d8eeb7e7593eb64
h0pbeat <arekquest@gmail.com>
2013-07-11 19:21:40 +0200

    named register url

--- sensible_data_platform/accounts/urls.py
+++ sensible_data_platform/accounts/urls.py
@@ -5,8 +5,7 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-   # url(r'^login/$', 'django.contrib.auth.views.login', name = 'login'),
     url(r'^login/$', 'accounts.views.do_login', name = 'login'),
     url(r'^logout/$', 'accounts.session.logout', name = 'logout'),
-    url(r'^register/$', 'accounts.register.register'),
+    url(r'^register/$', 'accounts.register.register', name = 'register'),
 )
