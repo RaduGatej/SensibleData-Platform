9526c570e867839acd8818453487479b3e0ac5c2
h0pbeat <arekquest@gmail.com>
2013-07-14 14:14:09 +0000

    adding deployment stuff

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 apt-get install python-pip
 pip install -r requirements.txt
 
-python sensible_data_platform/manage.py runserver 166.78.249.214:9081
+python sensible_data_platform/manage.py runserver $1:$2
