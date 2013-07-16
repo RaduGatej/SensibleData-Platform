d39a9e8833d07f538e124b743f923ceb938d1f4e
cardoppler <cardoppler@gmail.com>
2013-07-16 15:44:18 +0000

    extra added

--- sensible_data_platform/accounts/models.py
+++ sensible_data_platform/accounts/models.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import User
 class Participant(models.Model):
     user = models.OneToOneField(User)
     pseudonym = models.CharField(max_length=100, blank=True)
+
+class Extra(models.Model):
+    user = models.OneToOneField(User)
+    phone = models.CharField(max_length=100, blank=True)
