cb1530cd05f09a64a0f0d831852197bd95915ead
h0pbeat <arekquest@gmail.com>
2013-07-17 18:40:57 +0200

    fixed the url for deployment

--- sensible_data_platform/utils/platform_config.py
+++ sensible_data_platform/utils/platform_config.py
@@ -1,11 +1,9 @@
-#PLATFORM_URI = 'https://www.sensible.dtu.dk/sensible-data'
-PLATFORM_URI = 'http://166.78.249.214:9081'
+PLATFORM_URI = 'https://www.sensible.dtu.dk/sensible-data'
 
 
 IDENTITY_PROVIDERS = {
 	'CAS':	{
 		'connector': 'cas.py',
 		'auth_uri': 'https://auth.dtu.dk/dtu/',
-		'endpoint': '/identity_providers/cas/',
 	}
 }
