ed7fc327bb102d10736aa530b72f6dec1692d146
h0pbeat <arekquest@gmail.com>
2013-07-31 21:08:02 +0000

    logout from services

--- sensible_data_platform/accounts/session.py
+++ sensible_data_platform/accounts/session.py
@@ -4,14 +4,22 @@ from django.template import RequestContext
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
-from oauth2app.models import Client, AccessRange
+import urllib2
+from oauth2app.models import Client, AccessRange, ClientType, AccessToken
 
 def logout(request):
-    	next = request.REQUEST.get('next', '')
+	next = request.REQUEST.get('next', '')
 	
 	if request.user.is_authenticated:
+		try:
+			user = request.user
+			for client in Client.objects.filter(type=ClientType.objects.get(type='study')):
+				urllib2.urlopen(client.api_uri+'logout/?access_token=%s'%AccessToken.objects.filter(user=user, scope=AccessRange.objects.get(key='enroll'), client=client)[0].token)
+		except: pass
+
+
 		auth.logout(request)
+
     
 	if next == '':
 		return render_to_response('logout.html', {}, RequestContext(request))
