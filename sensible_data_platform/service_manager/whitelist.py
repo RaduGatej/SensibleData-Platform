4e7fcf8772d59a94638d399219f51bd49a1a156a
h0pbeat <arekquest@gmail.com>
2013-08-23 15:43:05 +0200

    logic for researcher registration in whitelist

--- sensible_data_platform/service_manager/whitelist.py
+++ sensible_data_platform/service_manager/whitelist.py
@@ -6,6 +6,16 @@ def checkWhitelist(user, client):
 	if client.name == 'SensibleDTU':
 		try: white_list = json.loads(open(settings.ROOT_DIR+'service_manager/SECURE_'+client.name+'_whitelist').read())
 		except: return True
+		
+		researcher_whitelist = []
+		try: 
+			researcher_whitelist = json.loads(open(settings.ROOT_DIR+'service_manager/SECURE_'+client.name+'_research_whitelist').read())
+		except: pass
+
+		try: 
+			if user.email in researcher_whitelist: return True
+		except: pass
+
 		try: user.cas.student_id
 		except Cas.DoesNotExist: return False
 		
