d01cdeaf8a32e23a941b00f67b5feb4e068de5f3
Lasse Valentini Jensen <lasse.valentini@gmail.com>
2013-11-18 13:49:30 +0000

    Added socialicious font for social icons on login page. Added a stylesheets-block to base-no-jq

--- sensible_data_platform/accounts_social/accounts_social.py
+++ sensible_data_platform/accounts_social/accounts_social.py
@@ -2,30 +2,15 @@ from django.core.context_processors import csrf
 from django.contrib.auth.models import User
 from social.apps.django_app.utils import BACKENDS
 from social.backends import utils as social_utils
-# from identity_providers.models import Cas
-
-# from openid_provider.models import *
-
-
-# def getUser(user):
-# 	values = {}
-# 	values['username'] = str(user)
-	
-# 	try:
-# 		student = Cas.objects.get(user=user) 
-# 		values['student_id'] = str(student.student_id)
-# 	except Cas.DoesNotExist: pass
-	
-# 	return values
+from itertools import chain
 
 ## A dict with backend names and "pretty" names
 backend_translation = {
-    'facebook':      'Facebook',
-    'facebook-app':  'Facebook',
-    'twitter':       'Twitter',
-    'google-oauth2': 'Google',
+    'facebook':      {'name': 'Facebook', 'icon': 'facebook',   'btnstyle':'facebook'},
+    'twitter':       {'name': 'Twitter',  'icon': 'twitter',    'btnstyle':'twitter'},
+    'google-oauth2': {'name': 'Google',   'icon': 'googleplus', 'btnstyle':'google'},
 }
 
 def getSocialProviders():
     backends = social_utils.load_backends(BACKENDS)
-    return [{'backend': backend, 'name': backend_translation[backend]} for backend in backends]
+    return [dict(chain({'backend': backend}.iteritems(), backend_translation[backend].iteritems())) for backend in backends]
--- sensible_data_platform/sensible_data_platform/settings.py
+++ sensible_data_platform/sensible_data_platform/settings.py
@@ -220,7 +220,7 @@ SOCIAL_AUTH_PIPELINE =(
     'social.pipeline.social_auth.auth_allowed',
     'social.pipeline.social_auth.social_user',
     'social.pipeline.user.get_username',
-    'accounts.social_pipeline.register',
+    'accounts_social.pipeline.register',
     'social.pipeline.social_auth.associate_user',
     'social.pipeline.social_auth.load_extra_data',
     'social.pipeline.user.user_details'
