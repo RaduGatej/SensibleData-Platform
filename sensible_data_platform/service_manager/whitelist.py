6b920b3180abccd3c12e175e2b5c2a593da38f74
h0pbeat <arekquest@gmail.com>
2014-02-02 11:37:50 +0100

    new whitelists added

--- sensible_data_platform/service_manager/whitelist.py
+++ sensible_data_platform/service_manager/whitelist.py
@@ -7,6 +7,16 @@ def checkWhitelist(user, client):
 		try: white_list = json.loads(open(settings.ROOT_DIR+'service_manager/SECURE_'+client.name+'_whitelist').read())
 		except: return True
 		
+		try: white_list_semester_3_4 = json.loads(open(settings.ROOT_DIR+'service_manager/SECURE_'+client.name+'_whitelist_semsester_3_4').read())
+		except: pass
+		
+		try: white_list_semester_5_6 = json.loads(open(settings.ROOT_DIR+'service_manager/SECURE_'+client.name+'_whitelist_semsester_5_6').read())
+		except: pass
+		
+
+		try: white_list_ku = json.loads(open(settings.ROOT_DIR+'service_manager/SECURE_'+client.name+'_whitelist_ku').read())
+		except: pass
+
 		researcher_whitelist = []
 		try: 
 			researcher_whitelist = json.loads(open(settings.ROOT_DIR+'service_manager/SECURE_'+client.name+'_research_whitelist').read())
@@ -24,12 +34,18 @@ def checkWhitelist(user, client):
 		try: 
 			if user.email in developer_whitelist: return True
 		except: pass
+		
+		try: 
+			if user.email in white_list_ku: return True
+		except: pass
 
 		try: user.cas.student_id
 		except Cas.DoesNotExist: return False
 		
 		
 		if user.cas.student_id in white_list: return True
+		if user.cas.student_id in white_list_semester_3_4: return True
+		if user.cas.student_id in white_list_semester_5_6: return True
 		
 		return False
 	return True #no whitelist registered, allow everyone
